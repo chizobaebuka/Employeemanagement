8e7fd2ca7a98c8515cf808d0e56d5bc74058907b
Decagon <decagon@Decagons-MacBook-Air.local>
2023-07-03 00:43:23 +0100

    login form styled and set up

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,20 +1,26 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+// import React from 'react';
 
 function Login() {
-  return (
-    <div className='d-flex justify-content-center align-items-center vh-100'>
-        <div className='bg-white p-3 rounded w-25'>
-            <h2>Login</h2>
-            <form>
-                <div className='mb-3'>
-                    <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' name='email' className='form-control rounded-0'/>
-                </div>
-            </form>
+    return (
+        <div className='d-flex justify-content-center align-items-center vh-100'>
+            <div className='bg-white p-3 rounded w-25'>
+                <h2>Login</h2>
+                <form>
+                    <div className='mb-3'>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder='Enter Email' name='email' className='form-control rounded-0' />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder='Enter Password' name='email' className='form-control rounded-0' />
+                    </div>
+                    <button type="submit" className='btn btn-success w-100 rounded-0'>Log in</button>
+                    <p>Agree to our terms and services</p>
+                    <button className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
